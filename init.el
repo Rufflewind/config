e3672103bfe9c268b599118f621125eea16d1e5a
Phil Ruffwind <rf@rufflewind.com>
2014-03-04 23:26:26 -0500

    Added *.yml to yaml extensions

--- init.el
+++ init.el
@@ -400,7 +400,7 @@
 
 ;; YAML
 (autoload 'yaml-mode "yaml-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
 
 ;; ---------------------------------------------------------------------------
 ;;
