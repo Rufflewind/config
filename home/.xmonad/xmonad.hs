35d704f32d60dc688ac0596b6c59c68958005f38
Phil Ruffwind <rf@rufflewind.com>
2014-05-16 15:13:59 -0400

    customize xmonad

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,8 +1,28 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import qualified Data.Map as Map
+-- import System.IO
 
-main =
-  xmonad defaultConfig
-  { terminal    = "urxvt"
-  , modMask     = mod4Mask
+main = xmonad =<< statusBar "xmobar" myPP toggleStatusKey myConfig
+
+myConfig = defaultConfig
+  { modMask = mod4Mask -- use Super instead of Alt
+  , terminal = "urxvt"
   , borderWidth = 3
+  , normalBorderColor = "black"
+  , focusedBorderColor = "#33ff7b"
+  , keys = \ x -> Map.union (Map.fromList $ newKeys x) (keys defaultConfig x)
   }
+
+myPP = defaultPP
+  { ppCurrent = wrapFc "#ffff00"
+  }
+
+toggleStatusKey XConfig { modMask = modMask } = (modMask, xK_b)
+
+wrapFc fc = wrap ("<fc=" ++ fc ++ ">") "</fc>"
+
+-- Add new and/or redefine key bindings
+newKeys (XConfig { modMask = modMask }) =
+  [ ((modMask, xK_q), spawn "killall xmobar; xmonad --recompile; xmonad --restart")
+  ]
