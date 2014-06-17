e46c9d0eae5e429e10b532e3c7a026e82c2b6747
Phil Ruffwind <rf@rufflewind.com>
2014-06-16 20:10:39 -0400

    switch from xmobar to dzen2

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,28 +1,49 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+import Text.Printf
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
 import qualified Data.Map as Map
--- import System.IO
 
-main = xmonad =<< statusBar "xmobar" myPP toggleStatusKey myConfig
+main = do
+  dzen <- spawnPipe myBar
+  xmonad $ defaultConfig
+    { modMask = mod4Mask                -- use Super instead of Alt
+    , terminal = "urxvt"
+    , borderWidth = 3
+    , normalBorderColor = "black"
+    , focusedBorderColor = "#33ff7b"
+    , keys = \ x -> Map.union (Map.fromList $ myKeys x)
+                              (keys defaultConfig x)
+    , logHook = myLogHook dzen >> fadeInactiveLogHook 0xdddddddd
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    }
 
-myConfig = defaultConfig
-  { modMask = mod4Mask -- use Super instead of Alt
-  , terminal = "urxvt"
-  , borderWidth = 3
-  , normalBorderColor = "black"
-  , focusedBorderColor = "#33ff7b"
-  , keys = \ x -> Map.union (Map.fromList $ newKeys x) (keys defaultConfig x)
-  }
+barFg = "#aaaaaa"
+barBg = "#222222"
+barFont = "inconsolata"
+barHeight = 16
 
-myPP = defaultPP
-  { ppCurrent = wrapFc "#ffff00"
-  }
+myBar = printf "dzen2 -ta l -fg '%s' -bg '%s' -fn %s -h %d"
+        barFg barBg barFont (barHeight :: Int)
 
-toggleStatusKey XConfig { modMask = modMask } = (modMask, xK_b)
+myLogHook h = dynamicLogWithPP $ defaultPP
+    { ppCurrent         = dzenColor "#ffffff" barBg . pad
+    , ppVisible         = dzenColor "#259f54" barBg . pad
+    , ppHidden          = dzenColor "#557e84" barBg . pad
+    , ppHiddenNoWindows = dzenColor "#444444" barBg . pad
+    , ppUrgent          = dzenColor "#ff3311" barBg . pad
+    , ppWsSep           = " "
+    , ppSep             = "  |  "
+    , ppLayout          = dzenColor "#259f54" barBg
+    , ppTitle           = (" " ++) . dzenColor "white" barBg . dzenEscape
+    , ppOutput          = hPutStrLn h
+    }
 
-wrapFc fc = wrap ("<fc=" ++ fc ++ ">") "</fc>"
-
--- Add new and/or redefine key bindings
-newKeys (XConfig { modMask = modMask }) =
-  [ ((modMask, xK_q), spawn "killall xmobar; xmonad --recompile; xmonad --restart")
+myKeys (XConfig { modMask = modMask }) =
+  [ ((modMask, xK_q),
+     spawn "killall dzen2; xmonad --recompile; xmonad --restart")
   ]
