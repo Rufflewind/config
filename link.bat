baac31114c06b6580ae4332380d10c8b92e42802
Phil Ruffwind <rf@rufflewind.com>
2014-08-11 23:06:53 -0400

    update Windows link script

--- link.bat
+++ link.bat
@@ -1,4 +1,13 @@
+@echo off
 cd %HOMEDRIVE%%HOMEPATH%
-mklink /j bin           stuff\boot\home\bin
-mklink    .bashrc       stuff\boot\home\.bashrc
-mklink    .bash_profile stuff\boot\home\.bash_profile
+set RELDIR=stuff\config
+rmdir     bin
+mklink /d bin                 %RELDIR%\home\bin
+del       .bashrc
+mklink    .bashrc             %RELDIR%\home\.bashrc
+del       .bash_profile
+mklink    .bash_profile       %RELDIR%\home\.bash_profile
+del       .emacs.d\init.el
+mklink    .emacs.d\init.el    ..\%RELDIR%\home\.emacs.d\init.el
+rmdir     .emacs.d\elisp
+mklink /d .emacs.d\elisp      ..\%RELDIR%\home\.emacs.d\elisp
