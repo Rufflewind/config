2c456471a91224b2bbf66b4a3bb6ad1b6d9f3d74
Phil Ruffwind <rf@rufflewind.com>
2014-10-20 16:25:04 -0400

    home/.emacs.d/init.el: remove ad hoc C++11 support

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -367,56 +367,8 @@
         (arglist-close . 0)
         (inextern-lang . 0)
         (innamespace . 0)))
-(add-hook
- 'c++-mode-hook
- '(lambda ()
-    (c-toggle-electric-state -1)
-    ;; We could place some regexes into `c-mode-common-hook', but note that
-    ;; their evaluation order matters.
-    (font-lock-add-keywords
-     nil
-     `(;; complete some fundamental keywords
-       (,(concat
-          "\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long"
-          "\\|float\\|double\\)\\>")
-        . font-lock-keyword-face)
-       ;; namespace names and tags - these are rendered as constants by cc-mode
-       ("\\<\\(\\w+::\\)" . font-lock-function-name-face)
-       ;;  new C++11 keywords
-       (,(concat
-          "\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr"
-          "\\|static_assert\\|thread_local\\|override\\|final\\)\\>")
-        . font-lock-keyword-face)
-       ("\\<\\(char16_t\\|char32_t\\)\\>" . font-lock-keyword-face)
-       ;; PREPROCESSOR_CONSTANT, PREPROCESSORCONSTANT
-       ("\\<[A-Z]*_[A-Z_]+\\>" . font-lock-constant-face)
-       ("\\<[A-Z]\\{3,\\}\\>"  . font-lock-constant-face)
-       ;; hexadecimal numbers
-       ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
-       ;; integer/float/scientific numbers
-       ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>"
-        . font-lock-constant-face)
-       ;; c++11 string literals
-       ;;       L"wide string"
-       ;;       L"wide string with UNICODE codepoint: \u2018"
-       ;;       u8"UTF-8 string", u"UTF-16 string", U"UTF-32 string"
-       ("\\<\\([LuU8]+\\)\".*?\"" 1 font-lock-keyword-face)
-       ;;       R"(user-defined literal)"
-       ;;       R"( a "quot'd" string )"
-       ;;       R"delimiter(The String Data" )delimiter"
-       ;;       R"delimiter((a-z))delimiter" is equivalent to "(a-z)"
-       ("\\(\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\)"
-        1 font-lock-keyword-face t) ; start delimiter
-       (,(concat
-          "\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}"
-          "(\\(.*?\\))[^\\s-\\\\()]\\{0,16\\}\"")
-        1 font-lock-string-face t)  ; actual string
-       (,(concat "\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}"
-                 "(.*?\\()[^\\s-\\\\()]\\{0,16\\}\"\\)")
-        1 font-lock-keyword-face t) ; end delimiter
-       ;; user-defined types (rather project-specific)
-       ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(type\\|ptr\\)\\>" . font-lock-type-face)
-       ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)))) t)
+(add-hook 'c++-mode-hook
+          '(lambda () (c-toggle-electric-state -1)) t)
 
 ;; CSS
 (add-to-list 'auto-mode-alist '("\\.\\(le\\|sc\\)ss\\'" . css-mode))
