7de5c554ec5d0487cfd029d82d6f168b6bf64de4
Phil Ruffwind <rf@rufflewind.com>
2015-09-16 13:23:09 -0400

    misc.sh: miscellaneous things to install

---
+++ misc.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -fsS -o "$HOME/.config/inkscape/palettes/Material_Design.gpl" \
+    https://raw.githubusercontent.com/dracs89/Material-Design-Color-Palette-for-Inkscape/master/Material_Design.gpl
