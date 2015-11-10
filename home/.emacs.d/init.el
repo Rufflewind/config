3daccf75bc02ec48d65e80ae3ad56de9300a798f
Phil Ruffwind <rf@rufflewind.com>
2015-11-10 06:09:07 -0500

    ~/.emacs.d/init.el: haskell-mode: indentation -> simple-indent

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -438,7 +438,7 @@
 ;; Haskell
 (autoload 'haskell-mode "haskell-mode-autoloads" nil t)
 (add-to-list 'auto-mode-alist '("\\.\\(c?hs\\|hsc\\)\\'" . haskell-mode))
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
 ;; JavaScript
 (add-to-list 'auto-mode-alist '("\\.\\(g\\|j\\)s\\'" . js-mode))
