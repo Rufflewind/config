90aa579ff6e62d0d0250b68672597c269e7ccbcc
Phil Ruffwind <rf@rufflewind.com>
2020-03-01 18:03:56 -0800

    Add fix-links and remove broken symlinks

--- link.bat
+++ link.bat
@@ -13,5 +13,3 @@ mklink    .profile                           %base%\home\.profile
 mkdir     .emacs.d\elisp
 del       .emacs.d\init.el
 mklink    .emacs.d\init.el                 ..\%base%\home\.emacs.d\init.el
-del       .emacs.d\elisp\guess-style.el
-mklink    .emacs.d\elisp\guess-style.el ..\..\%base%\home\.emacs.d\elisp\guess-style.el
