14c555c96f019bb85e67f4b7a0e57ff6b281ab4d
Phil Ruffwind <rf@rufflewind.com>
2020-05-02 15:44:22 -0700

    Update ~/sbin/reflector.sh

--- home/sbin/reflector.sh
+++ home/sbin/reflector.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-exec sudo reflector --verbose --score 10 --sort rate --save /etc/pacman.d/mirrorlist
+tmp=`mktemp`
+reflector --verbose --country="United States" --score=10 --sort=rate | tee "$tmp"
+cat >&2 <<EOF
+========================================
+To accept these changes, run:
+
+    sudo cp "$tmp" /etc/pacman.d/mirrorlist && rm "$tmp"
+
+EOF
